3f577a9462fca1641967919ee8f2577a1d3b3eca
XAJX <88792057+XAJX179@users.noreply.github.com>
2024-07-21 23:25:06 +0530

    Feat : Add A Button

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const div = document.querySelector("div");
 
+const btn = document.querySelector("button");
+
 const divs = [];
 function createDivs(line) {
   for (let i = 0; i < line; i++) {
@@ -8,7 +10,28 @@ function createDivs(line) {
       const divGrid = document.createElement("div");
       divLine.appendChild(divGrid);
       div.appendChild(divLine);
+
+      divGrid.addEventListener("mouseover", () => {
+        divGrid.style.background = "black";
+      });
     }
   }
 }
 createDivs(16);
+
+function removeGrids() {
+  while (div.firstElementChild) {
+    div.removeChild(div.firstElementChild);
+  }
+}
+
+function askUser() {
+  let size = +prompt("Enter Size: ");
+  if (size <= 100 && size > 0) {
+    removeGrids();
+    createDivs(size);
+  } else {
+    alert("Enter between 1 and 100");
+  }
+}
+btn.addEventListener("click", askUser);
